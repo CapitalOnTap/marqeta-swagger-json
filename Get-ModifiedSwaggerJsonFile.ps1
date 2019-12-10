6defd3bba6e39d3f33b72af72404098558aa3de7
John Cyril <john.cyril@capitalontap.com>
2019-12-10 10:07:30 +0000

    added country_code to swagger.json

--- Get-ModifiedSwaggerJsonFile.ps1
+++ Get-ModifiedSwaggerJsonFile.ps1
@@ -752,17 +752,21 @@ $jsonObject.definitions['issuer_processor'] = $issuerProcessorSchema
 
 Write-Verbose "Fixing incorrectly named definition properties."
 
-# transaction_model
-#   issuer_interchange_amount
-$jsonObject.definitions['transaction_model'].properties.Remove('issuerInterchangeAmount') | Out-Null
-$jsonObject.definitions['transaction_model'].properties.Add('issuer_interchange_amount', @{ 'type' = 'number' })
-#   issuer_received_time
-$jsonObject.definitions['transaction_model'].properties.Remove('issuerReceivedTime') | Out-Null
-$jsonObject.definitions['transaction_model'].properties.Add('issuer_received_time', @{ 'type' = 'string' })
-#   issuer_payment_node
-$jsonObject.definitions['transaction_model'].properties.Remove('issuerPaymentNode') | Out-Null
-$jsonObject.definitions['transaction_model'].properties.Add('issuer_payment_node', @{ 'type' = 'string' })
-
+    # transaction_model
+    #   issuer_interchange_amount
+    $jsonObject.definitions['transaction_model'].properties.Remove('issuerInterchangeAmount') | Out-Null
+    $jsonObject.definitions['transaction_model'].properties.Add('issuer_interchange_amount', @{ 'type' = 'number' })
+    #   issuer_received_time
+    $jsonObject.definitions['transaction_model'].properties.Remove('issuerReceivedTime') | Out-Null
+    $jsonObject.definitions['transaction_model'].properties.Add('issuer_received_time', @{ 'type' = 'string' })
+    #   issuer_payment_node
+    $jsonObject.definitions['transaction_model'].properties.Remove('issuerPaymentNode') | Out-Null
+    $jsonObject.definitions['transaction_model'].properties.Add('issuer_payment_node', @{ 'type' = 'string' })
+	
+	# transaction_card_acceptor
+	#   country_code
+	$jsonObject.definitions['transaction_card_acceptor'].properties.Remove('country') | Out-Null
+    $jsonObject.definitions['transaction_card_acceptor'].properties.Add('country_code', @{ 'type' = 'string' })
     
 #
 # /Definitions: incorrectly named properties
